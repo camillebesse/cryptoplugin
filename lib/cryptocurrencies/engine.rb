c787647deded3a002095e4441cb7595c5d47e682
Joe Buhlig <hello@joebuhlig.com>
2017-11-16 15:47:52 -0600

    Error catch on topic search

--- lib/cryptocurrencies/engine.rb
+++ lib/cryptocurrencies/engine.rb
@@ -39,22 +39,24 @@ module Cryptocurrencies
                   topic = Topic.find(topic_cfs[0].topic_id)
                 end
 
-                topic.custom_fields["cryptocurrency_id"] = currency["id"]
-                topic.custom_fields["cryptocurrency_name"] = currency["name"]
-                topic.custom_fields["cryptocurrency_symbol"] = currency["symbol"]
-                topic.custom_fields["cryptocurrency_rank"] = currency["rank"]
-                topic.custom_fields["cryptocurrency_price_usd"] = currency["price_usd"]
-                topic.custom_fields["cryptocurrency_price_btc"] = currency["price_btc"]
-                topic.custom_fields["cryptocurrency_24h_volume_usd"] = currency["24h_volume_usd"]
-                topic.custom_fields["cryptocurrency_market_cap_usd"] = currency["market_cap_usd"]
-                topic.custom_fields["cryptocurrency_available_supply"] = currency["available_supply"]
-                topic.custom_fields["cryptocurrency_total_supply"] = currency["total_supply"]
-                topic.custom_fields["cryptocurrency_max_supply"] = currency["max_supply"]
-                topic.custom_fields["cryptocurrency_percent_change_1h"] = currency["percent_change_1h"]
-                topic.custom_fields["cryptocurrency_percent_change_24h"] = currency["percent_change_24h"]
-                topic.custom_fields["cryptocurrency_percent_change_7d"] = currency["percent_change_7d"]
-                topic.custom_fields["cryptocurrency_last_updated"] = currency["last_updated"]
-                topic.save
+                if topic
+                  topic.custom_fields["cryptocurrency_id"] = currency["id"]
+                  topic.custom_fields["cryptocurrency_name"] = currency["name"]
+                  topic.custom_fields["cryptocurrency_symbol"] = currency["symbol"]
+                  topic.custom_fields["cryptocurrency_rank"] = currency["rank"]
+                  topic.custom_fields["cryptocurrency_price_usd"] = currency["price_usd"]
+                  topic.custom_fields["cryptocurrency_price_btc"] = currency["price_btc"]
+                  topic.custom_fields["cryptocurrency_24h_volume_usd"] = currency["24h_volume_usd"]
+                  topic.custom_fields["cryptocurrency_market_cap_usd"] = currency["market_cap_usd"]
+                  topic.custom_fields["cryptocurrency_available_supply"] = currency["available_supply"]
+                  topic.custom_fields["cryptocurrency_total_supply"] = currency["total_supply"]
+                  topic.custom_fields["cryptocurrency_max_supply"] = currency["max_supply"]
+                  topic.custom_fields["cryptocurrency_percent_change_1h"] = currency["percent_change_1h"]
+                  topic.custom_fields["cryptocurrency_percent_change_24h"] = currency["percent_change_24h"]
+                  topic.custom_fields["cryptocurrency_percent_change_7d"] = currency["percent_change_7d"]
+                  topic.custom_fields["cryptocurrency_last_updated"] = currency["last_updated"]
+                  topic.save
+                end
               end
             end
 
